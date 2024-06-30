ad793b3fbfa5cc7665f74d4e5e79f8e207fd1045
mtklar <49459830+mtklar@users.noreply.github.com>
2024-06-30 21:37:32 +0200

    Added save file

---
+++ content_scripts/to_save.js
@@ -0,0 +1,34 @@
+const myPart = {}
+
+myPart.quanta = () => {
+  var data = []
+  const titles = ["Realised Profit", "Unrealised Profit", "Total Profit", "Unit Cost", "ROI", "Capital Required", "Max Buys", "Remaining Cost", "Remaining Amount"]
+  var datafields = document.querySelectorAll("div[class^='valueItem-'][class*='blockHidden-']")
+  for (const el of datafields) {
+    let txt = el.innerHTML
+    var match = txt.match('title="(.*?)"')
+    if (match && titles.includes(match[1])) {
+      var dataEl = { "name": match[1], "value": el.innerText }
+      console.log(dataEl)
+      data.push(dataEl)
+    }
+  }
+  return data
+}
+
+var report = {}
+var titles = ["Realised Profit", "Unrealised Profit", "Total Profit", "Unit Cost", "ROI", "Capital Required", "Max Buys", "Remaining Cost", "Remaining Amount"]
+var datafields = document.querySelectorAll("div[class^='valueItem-'][class*='blockHidden-']")
+for (const el of datafields) {
+  let txt = el.innerHTML
+  var match = txt.match('title="(.*?)"')
+  if (match && titles.includes(match[1])) {
+    var numberStr = el.innerText
+    if (numberStr[0] == '−') {
+      report[match[1]] = parseFloat(numberStr.replace(/−/g, '').replace(/,/g, '')) * -1
+    } else {
+      report[match[1]] = parseFloat(numberStr.replace(/,/g, ''))
+    }
+  }
+}
+
