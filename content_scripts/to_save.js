2d0f04eedfceb9afdd04c5ddf71090bab1f60b90
Matthias Klar <49459830+mtklar@users.noreply.github.com>
2025-05-17 09:19:23 +0200

    Fixed detection of values in TV

--- content_scripts/to_save.js
+++ content_scripts/to_save.js
@@ -1,34 +1,58 @@
-const myPart = {}
+const myPart = {};
 
 myPart.quanta = () => {
-  var data = []
-  const titles = ["Realised Profit", "Unrealised Profit", "Total Profit", "Unit Cost", "ROI", "Capital Required", "Max Buys", "Remaining Cost", "Remaining Amount"]
-  var datafields = document.querySelectorAll("div[class^='valueItem-'][class*='blockHidden-']")
+  var data = [];
+  const titles = [
+    "Realised Profit",
+    "Unrealised Profit",
+    "Total Profit",
+    "Unit Cost",
+    "ROI",
+    "Capital Required",
+    "Max Buys",
+    "Remaining Cost",
+    "Remaining Amount",
+  ];
+  var datafields = document.querySelectorAll(
+    "div[class^='valueItem-'][class*='valueValue-']",
+  );
   for (const el of datafields) {
-    let txt = el.innerHTML
-    var match = txt.match('title="(.*?)"')
+    let txt = el.innerHTML;
+    var match = txt.match('title="(.*?)"');
     if (match && titles.includes(match[1])) {
-      var dataEl = { "name": match[1], "value": el.innerText }
-      console.log(dataEl)
-      data.push(dataEl)
+      var dataEl = { name: match[1], value: el.innerText };
+      console.log(dataEl);
+      data.push(dataEl);
     }
   }
-  return data
-}
+  return data;
+};
 
-var report = {}
-var titles = ["Realised Profit", "Unrealised Profit", "Total Profit", "Unit Cost", "ROI", "Capital Required", "Max Buys", "Remaining Cost", "Remaining Amount"]
-var datafields = document.querySelectorAll("div[class^='valueItem-'][class*='blockHidden-']")
+var report = {};
+var titles = [
+  "Realised Profit",
+  "Unrealised Profit",
+  "Total Profit",
+  "Unit Cost",
+  "ROI",
+  "Capital Required",
+  "Max Buys",
+  "Remaining Cost",
+  "Remaining Amount",
+];
+var datafields = document.querySelectorAll(
+  "div[class^='valueItem-'][class*='valueValue-']",
+);
 for (const el of datafields) {
-  let txt = el.innerHTML
-  var match = txt.match('title="(.*?)"')
+  let txt = el.innerHTML;
+  var match = txt.match('title="(.*?)"');
   if (match && titles.includes(match[1])) {
-    var numberStr = el.innerText
-    if (numberStr[0] == '−') {
-      report[match[1]] = parseFloat(numberStr.replace(/−/g, '').replace(/,/g, '')) * -1
+    var numberStr = el.innerText;
+    if (numberStr[0] == "−") {
+      report[match[1]] =
+        parseFloat(numberStr.replace(/−/g, "").replace(/,/g, "")) * -1;
     } else {
-      report[match[1]] = parseFloat(numberStr.replace(/,/g, ''))
+      report[match[1]] = parseFloat(numberStr.replace(/,/g, ""));
     }
   }
 }
-
